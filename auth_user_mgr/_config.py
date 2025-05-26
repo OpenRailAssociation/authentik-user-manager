57000a0312827092c25864b3f763bfa485a4f364
Max Mehl <max.mehl@deutschebahn.com>
2025-05-26 16:44:30 +0200

    make a sanity check against the app config

--- auth_user_mgr/_config.py
+++ auth_user_mgr/_config.py
@@ -21,6 +21,27 @@ def read_yaml_config_file(path: str) -> dict:
 
 def read_app_and_users_config(app_config_path: str, user_config_path: str) -> tuple[dict, dict]:
     """Read app and user config files and return a tuple of dicts"""
+    # Load the app and user config files
     app_config = read_yaml_config_file(app_config_path)
     user_config = read_yaml_config_file(user_config_path)
+
+    # Check if the configs contain all required keys
+    cfg_sanity_required_keys(
+        cfg=app_config,
+        required_keys=[
+            "authentik_url",
+            "authentik_token",
+            "authentik_title",
+            "invitation_flow_slug",
+        ],
+    )
+
     return app_config, user_config
+
+
+def cfg_sanity_required_keys(cfg: dict, required_keys: list[str]) -> None:
+    """Check if the config contains all required keys"""
+    missing_keys = [key for key in required_keys if key not in cfg]
+    if missing_keys:
+        raise KeyError(f"Config is missing required keys: {', '.join(missing_keys)}")
+    logging.debug("Config contains all required keys: %s", required_keys)
