46c3a5f34242aa17d50ee302554056fd8001bda0
Max Mehl <max.mehl@deutschebahn.com>
2025-07-04 13:58:00 +0200

    test: restart tests, starting with config sanity checks and some local test cases

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,18 +8,32 @@ import pytest
 from auth_user_mgr._config import read_app_and_users_config
 from auth_user_mgr._user import User
 
-CONFIG_APP_SAMPLE = "config/app.sample.yaml"
-CONFIG_USERS_SAMPLE = "config/users.sample.yaml"
+CONFIG_APP_SAMPLE = "tests/data/sample/app.sample.yaml"
+CONFIG_USERS_FILE_SAMPLE = "tests/data/sample/users.sample.yaml"
+CONFIG_USERS_DIR_SAMPLE = "tests/data/sample/users.sample"
 
 
-@pytest.fixture(name="sample_configs")
-def fixture_sample_configs():
+@pytest.fixture(name="sample_configs_userfile")
+def fixture_sample_configs_userfile():
     """
-    Fixture to load sample application and user configurations.
+    Fixture to load sample application and user configurations (single file).
     Returns:
         tuple: A tuple containing the application configuration and user configuration.
     """
-    app_config, users_config = read_app_and_users_config(CONFIG_APP_SAMPLE, CONFIG_USERS_SAMPLE)
+    app_config, users_config = read_app_and_users_config(
+        CONFIG_APP_SAMPLE, CONFIG_USERS_FILE_SAMPLE
+    )
+    return app_config, users_config
+
+
+@pytest.fixture(name="sample_configs_userdir")
+def fixture_sample_configs_userdir():
+    """
+    Fixture to load sample application and user configurations (directory).
+    Returns:
+        tuple: A tuple containing the application configuration and user configuration.
+    """
+    app_config, users_config = read_app_and_users_config(CONFIG_APP_SAMPLE, CONFIG_USERS_DIR_SAMPLE)
     return app_config, users_config
 
 
@@ -37,5 +51,6 @@ def fixture_sample_users(sample_configs):
     """
     _, users_config = sample_configs
     return [
-        User(name=u["name"], email=u["email"], configured_groups=u.get("groups", [])) for u in users_config
+        User(name=u["name"], email=u["email"], configured_groups=u.get("groups", []))
+        for u in users_config
     ]
